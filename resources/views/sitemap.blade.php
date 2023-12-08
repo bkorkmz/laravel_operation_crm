f5c2cce3574474a6f770fcd817ae6c105e59fde0
Burak Korkmaz <34342541+bkorkmz@users.noreply.github.com>
2023-12-08 17:34:58 +0300

    sitemap blade update

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,4 +1,22 @@
-<?xml version="1.0" encoding="UTF-8" ?>
+<?php
+echo '<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>';
+?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+        http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd
+        http://www.google.com/schemas/sitemap-image/1.1
+        http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd">
+    <url>
+
+
+        <loc>{{config('settings.site_url')}}</loc>
+        <lastmod>{{ date('Y-m-d') }}T{{date('h:i:s')}}+03:00</lastmod>
+        <changefreq>always</changefreq>
+        <priority>1</priority>
+    </url>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach ($urls as $url)
         <url>
