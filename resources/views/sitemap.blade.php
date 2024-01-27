861008a80450776ff49b58d5b228fb6d6959e4fd
Burak Korkmaz <34342541+bkorkmz@users.noreply.github.com>
2024-01-28 02:34:45 +0300

    hellonewmedia merge

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,7 +1,7 @@
 
 @php
     echo   "<?xml version='1.0' encoding='UTF-8'?>
-        <urlset
+      <urlset
         xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\"
         xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
         xsi:schemaLocation=\"http://www.sitemaps.org/schemas/sitemap/0.9
@@ -18,4 +18,4 @@
         <priority>{{$url['priority']}}</priority>
     </url>
     @endforeach
-    </urlset>
+</urlset>
